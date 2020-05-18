22b699bb470c0944c6ab8234fbfa5d342ca3acc2
Tek <tek@randhome.io>
2020-05-18 18:09:01 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pycrtsh',
-    version='0.3.0',
+    version='0.3.1',
     description='Python library to request crt.sh certificate information',
     long_description=long_description,
     long_description_content_type="text/markdown",
