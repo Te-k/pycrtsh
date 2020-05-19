8ab7dccfb7b53ae37304ccf73a154442b357d063
Tek <tek@randhome.io>
2020-05-19 12:25:56 +0200

    Update libraries versions

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='Tek',
     author_email='tek@randhome.io',
     keywords='security',
-    install_requires=['requests', 'lxml==4.2.6', 'beautifulsoup4==4.7.0', 'python-dateutil'],
+    install_requires=['requests', 'lxml==4.5.1', 'beautifulsoup4==4.9.1', 'python-dateutil'],
     license='MIT',
     packages=['pycrtsh'],
     entry_points= {
